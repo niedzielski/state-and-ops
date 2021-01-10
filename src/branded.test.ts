ff764382e573079258dc081f6632596873d6534d
Stephen Niedzielski <stephen@niedzielski.com>
2021-01-09 18:34:46 -0700

    Change branded example from XY to Int

---
+++ src/branded.test.ts
@@ -0,0 +1,60 @@
+import {Int} from './branded'
+
+describe('Int()', () => {
+  test.each([
+    ['min safe integer', Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER],
+    ['negative integer', -1, -1],
+    ['zero', 0, 0],
+    ['positive integer', 1, 1],
+    ['max safe integer', Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+
+    ['negative infinity', Number.NEGATIVE_INFINITY, Number.MIN_SAFE_INTEGER],
+    ['min fraction', -Number.MAX_VALUE, Number.MIN_SAFE_INTEGER],
+    ['negative fraction', -1.5, -1],
+    ['positive fraction', 1.5, 1],
+    ['max fraction', Number.MAX_VALUE, Number.MAX_SAFE_INTEGER],
+    ['positive infinity', Number.POSITIVE_INFINITY, Number.MAX_SAFE_INTEGER]
+  ])('%s', (_, value, expected) => expect(Int(value)).toStrictEqual(expected))
+
+  test('not a number', () => expect(() => Int(NaN)).toThrow())
+})
+
+describe('assert()', () => {
+  test.each([
+    ['min safe integer', Number.MIN_SAFE_INTEGER],
+    ['negative integer', -1],
+    ['zero', 0],
+    ['positive integer', 1],
+    ['max safe integer', Number.MAX_SAFE_INTEGER]
+  ])('%s', (_, value) => Int.assert(value))
+
+  test.each([
+    ['negative infinity', Number.NEGATIVE_INFINITY],
+    ['min fraction', -Number.MAX_VALUE],
+    ['negative fraction', -1.5],
+    ['positive fraction', 1.5],
+    ['max fraction', Number.MAX_VALUE],
+    ['positive infinity', Number.POSITIVE_INFINITY],
+    ['not a number', NaN]
+  ])('%s', (_, value) => expect(() => Int.assert(value)).toThrow())
+})
+
+describe('is()', () => {
+  test.each([
+    ['min safe integer', Number.MIN_SAFE_INTEGER],
+    ['negative integer', -1],
+    ['zero', 0],
+    ['positive integer', 1],
+    ['max safe integer', Number.MAX_SAFE_INTEGER]
+  ])('%s', (_, value) => expect(Int.is(value)).toStrictEqual(true))
+
+  test.each([
+    ['negative infinity', Number.NEGATIVE_INFINITY],
+    ['min fraction', -Number.MAX_VALUE],
+    ['negative fraction', -1.5],
+    ['positive fraction', 1.5],
+    ['max fraction', Number.MAX_VALUE],
+    ['positive infinity', Number.POSITIVE_INFINITY],
+    ['not a number', NaN]
+  ])('%s', (_, value) => expect(Int.is(value)).toStrictEqual(false))
+})
---
+++ src/branded.ts
@@ -0,0 +1,33 @@
+/**
+ * An integer `number` in
+ * [`Number.MIN_SAFE_INTEGER`, `Number.MAX_SAFE_INTEGER`].
+ */
+export type Int = number & {[brand]: void}
+declare const brand: unique symbol
+
+export function Int(val: number): Int {
+  const int = Int.tryTo(val)
+  if (int === undefined)
+    throw new Error(`${val} cannot be converted to an Int.`)
+  return int
+}
+
+// This can be made more generic as a constrained RangeInt / DomainInt / ...
+export namespace Int {
+  export function tryTo(value: number): Int | undefined {
+    if (Number.isNaN(value)) return
+    if (value > Number.MAX_SAFE_INTEGER) return <Int>Number.MAX_SAFE_INTEGER
+    if (value < Number.MIN_SAFE_INTEGER) return <Int>Number.MIN_SAFE_INTEGER
+    return <Int>Math.trunc(value)
+  }
+
+  // Although the brand is unavailable for runtime type discrimination,
+  // assertions can be used where wanted.
+  export function assert(value: number | Int): asserts value is Int {
+    if (!is(value)) throw new Error(`${value} is not an Int.`)
+  }
+
+  export function is(value: number | Int): value is Int {
+    return tryTo(value) === value
+  }
+}
